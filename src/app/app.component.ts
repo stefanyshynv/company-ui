33af7284b3cda82b7f213e26dc91da94c32e07c4
Vasyl Stefanyshyn <vtefanyshyn@liaisonedu.com>
2023-09-06 17:40:02 +0300

    Add backend call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'company-ui';
+  users = Array<object>;
+
+  constructor(private http: HttpClient) { }
+
+    ngOnInit() {      
+        // Simple GET request with response type <any>
+        this.http.get<any>('http://localhost:8080/users').subscribe(data => {
+            this.users = data;
+        })
+    }
 }
